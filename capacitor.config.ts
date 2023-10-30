7056bcd08ed51a6facaf5d720458f9f1bd1eb1b3
jaime904 <jai.aravena@duocuc.cl>
2023-10-30 17:52:18 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proye',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
